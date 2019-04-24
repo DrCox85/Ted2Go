053dba793b531475d2f2ecb94d83ae1a50f80014
SamFisher <christianwilss@gmail.com>
2019-04-24 20:00:15 +0200

    add MINGW64 for logo create on Windows

--- logo/update.bat
+++ logo/update.bat
@@ -1,4 +1,4 @@
 @echo off
-windres -v --target=pe-i386 resource.rc resource.o
-windres -v --target=pe-x86-64 resource.rc resource_x64.o
+mingw64\bin\windres -v --target=pe-i386 resource.rc resource.o
+mingw64\bin\windres -v --target=pe-x86-64 resource.rc resource_x64.o
 pause
